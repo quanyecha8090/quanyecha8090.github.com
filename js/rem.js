4d51910c92e79b0b8efea04a0ef792f461c9bb9a
liuyufang <fangjob184@sina.com>
2016-03-25 15:17:19 +0800

    ceshi

--- js/rem.js
+++ js/rem.js
@@ -1,3 +1,30 @@
-window.onload = window.onresize = function(){
-	document.documentElement.style.fontSize=document.documentElement.clientWidth*20/375+'px';
-};
+(function(){
+	window.onload=window.onresize=function(){
+		document.documentElement.style.fontSize=document.documentElement.clientWidth*20/375+'px';
+	};
+})();
+//document.addEventListener('DOMContentLoaded',function(){
+//	
+//	var btnShare = document.querySelector('#btn_share');
+//	var shareCon = document.querySelector('#shareCon');
+//	var btnClose = document.querySelector('#shareClose');
+//	var shareMask = document.querySelector('#mask');
+//	
+//	btnShare.addEventListener('click',shareShow,false);
+//	btnClose.addEventListener('click',shareHide,false);	
+//	shareMask.addEventListener('click',function(e){
+//		e.stopPropagation();
+//		shareHide();
+//	},false);
+//
+//	function shareShow(){
+//		shareMask.style.display = 'block';
+//		shareCon.style.height = '13.275rem';
+//	}
+//	function shareHide(){
+//		shareMask.style.display = 'none';
+//		shareCon.style.height = 0;
+//	}
+//},false);
+
+	
